2780242374dc7f1c847a0e50af2d50cf6b704f8e
robertlee2k <torobert@qq.com>
2016-06-16 13:56:05 +0800

    1

--- yueyueGo/src/yueyueGo/MLPClassifier.java
+++ yueyueGo/src/yueyueGo/MLPClassifier.java
@@ -24,22 +24,25 @@ public class MLPClassifier extends NominalClassifier {
 	//5. HS300 2008-2016 10-20单元格10%;中证500 20/30/50 7%-8%;全市场20/30/50 6%-8% ;  全市场整体胜率 37849/97947
 	// 参数：  eval 0.5 / 单独评估阀值/ TP——FP RATIO { 1.8, 1.5, 1.2, 1.0, 1.0 }, UPPer { 0.07, 0.09, 0.11, 0.15, 0.2 } TP_FP_BOTTOM_LINE=0.6
 	// lower_limit  { 0.01, 0.01, 0.01, 0.01,0.01 } DEFAULT_THRESHOLD=0.7
+	
+	// 6
+	//SAMPLE_UPPER_LIMIT  {0.07, 0.09, 0.1, 0.1, 0.1 }; TP_FP_RATIO_LIMIT {  1.6, 1.4, 1.3, 1.1, 0.9 };TP_FP_BOTTOM_LINE=0.7
 	public MLPClassifier() {
 		super();
 		classifierName="mlp";
 		inputAttShouldBeIndependent=true; //这个模型是用短格式的 		
 		m_policySubGroup = new String[]{"5","10","20","30","60" };
 		m_skipTrainInBacktest = true;
-		m_skipEvalInBacktest = true;
+		m_skipEvalInBacktest = false;
 		
 		EVAL_RECENT_PORTION = 0.7; // 计算最近数据阀值从历史记录中选取多少比例的最近样本
 		m_sepeperate_eval_HS300=true;//单独为HS300评估阀值
 		m_seperate_classify_HS300=true;
 		
-		SAMPLE_LOWER_LIMIT =new double[] { 0.01, 0.01, 0.01,0.01,0.01 }; // 各条均线选择样本的下限
-		SAMPLE_UPPER_LIMIT =new double[] { 0.07, 0.09, 0.1, 0.13, 0.15 }; // 各条均线选择样本的上限
-		TP_FP_RATIO_LIMIT=new double[] { 1.8, 1.5, 1.2, 1.0, 1.0 };//选择样本阀值时TP FP RATIO从何开始
-		TP_FP_BOTTOM_LINE=0.8; //TP/FP的下限
+		SAMPLE_LOWER_LIMIT =new double[] { 0.01, 0.01, 0.02, 0.02, 0.02 }; // 各条均线选择样本的下限
+		SAMPLE_UPPER_LIMIT =new double[] {0.07, 0.09, 0.1, 0.1, 0.1 }; // 各条均线选择样本的上限
+		TP_FP_RATIO_LIMIT=new double[] {  1.6, 1.4, 1.3, 1.1, 0.9 };//选择样本阀值时TP FP RATIO从何开始
+		TP_FP_BOTTOM_LINE=0.7; //TP/FP的下限
 		DEFAULT_THRESHOLD=0.7; // 找不出threshold时缺省值。
 	}
 	
@@ -47,9 +50,14 @@ public class MLPClassifier extends NominalClassifier {
 	public Classifier loadModel(String yearSplit, String policySplit) throws Exception{
 		//TODO 这是为MLP单独准备的模型，模型文件是按年读取，但evaluation文件不变仍按月
 		int inputYear=Integer.parseInt(yearSplit.substring(0,4));
-		//TODO 临时处理
-		if (inputYear>=2014 && inputYear<2016)
-			inputYear=2014;
+
+		//TODO 为2014和2016年增加一个模型
+		int inputMonth=Integer.parseInt(yearSplit.substring(4,6));
+		if (inputYear==2014 && inputMonth>6){
+			inputYear=201407;
+		}else if (inputYear==2016 && inputMonth==5) {
+			inputYear=201605;
+		}
 //		String modelYear=null;
 //		if (inputYear>=2008 && inputYear<2010)
 //			modelYear="2008";
